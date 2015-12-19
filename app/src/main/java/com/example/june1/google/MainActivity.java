c6c02509d8f3db2ecfb97455faf63b685b781a4c
jaljune <astrayakma@google.com>
2015-12-19 14:42:42 +0900

    5

---
+++ app/src/main/java/com/example/june1/google/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.june1.google;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+// AIzaSyAv7EAaKGqTf7-jDeb-8qV_JUe8pXQbSv8
+
+    static final LatLng SEOUL = new LatLng( 37.56, 126.97);
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker seoul = map.addMarker(new MarkerOptions().position(SEOUL).title("Seoul"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 15));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    }
+}
